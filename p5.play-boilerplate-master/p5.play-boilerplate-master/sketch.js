671ad6d5efb157154baabeb0e62fbbfede9112dc
yocool906 <74095298+yocool906@users.noreply.github.com>
2020-11-13 09:01:41 -0500

    Add files via upload

---
+++ p5.play-boilerplate-master/p5.play-boilerplate-master/sketch.js
@@ -0,0 +1,37 @@
+var car, wall;
+var speed, weight;
+function setup() {
+  createCanvas(1600,400);
+speed=random(55,90)
+weight=random(400,1500)
+ car=createSprite(50, 200, 50, 50);
+car.velocityX = speed;
+wall=createSprite(1500,200,60,height/2);
+wall.shapeColor=(80,80,80);
+}
+function draw() {
+  background("black");  
+if(wall.x-car.x < (car.width+wall.width)/2)
+{
+  car.velocityX=0;
+  var deformation=0.5 * weight *speed/22509;
+
+  if(deformation>180)
+  {
+    car.shapeColor="red"
+  }
+if(deformation<180 && deformation>100) {
+
+
+  
+  
+  car.shapeColor="pink"
+}
+
+  if(deformation<100)
+  {
+    car.shapeColor="blue"
+  }
+ 
+  drawSprites();
+}}
